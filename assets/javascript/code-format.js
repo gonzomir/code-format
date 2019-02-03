1e4498ab78172e4b0ae5b328d16239a2cc2b9893
Milen Petrinski <gonzo@greatgonzo.net>
2019-02-03 22:29:01 +0200

    Add keyboard shortcut (Ctrl+D).

--- assets/javascript/code-format.js
+++ assets/javascript/code-format.js
@@ -3,20 +3,32 @@
 	var select = wp.data.select;
 
 	var codeEdit = function( props ) {
-		return wp.element.createElement(
-			wp.editor.RichTextToolbarButton,
-			{
-				icon: 'editor-code',
-				title: __( 'Code', 'format-code' ),
-				onClick: function() {
-					props.onChange( wp.richText.toggleFormat(
-						props.value,
-						{ type: 'core/code' }
-					) );
-				},
-				isActive: props.isActive,
-			}
-		);
+		var onToggle = function(){
+			props.onChange( wp.richText.toggleFormat(
+				props.value,
+				{ type: 'core/code' }
+			) );
+		}
+
+		return [
+			wp.element.createElement(
+				wp.editor.RichTextShortcut,
+				{
+					type: "primary",
+					character: "d",
+					onUse: onToggle,
+				}
+			),
+			wp.element.createElement(
+				wp.editor.RichTextToolbarButton,
+				{
+					icon: 'editor-code',
+					title: __( 'Code', 'format-code' ),
+					onClick: onToggle,
+					isActive: props.isActive,
+				}
+			),
+		];
 	}
 
 	var code_format = select( 'core/rich-text' ).getFormatType( 'core/code' );
